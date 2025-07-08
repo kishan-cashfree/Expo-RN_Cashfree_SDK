384c559e3662d5778b27e742b508c5d669a03e3a
Kishan_maurya <kishan.maurya@cashfree.com>
2025-07-08 14:35:35 +0530

    Expo 51 : Cashfree SDK : Android & ios Tested

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Expo 53 EAS</Text>
+      <Text>Open up App.js to start working on your app! Expo 51</Text>
       <Button title="Start Payment" onPress={startPayment} />
       <StatusBar style="auto" />
     </View>
